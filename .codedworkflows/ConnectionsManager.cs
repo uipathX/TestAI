5e2c643d4ce2261321cd5f3aa398e26fdda12111
John Mungandi <johnxpres@gmail.com>
2024-12-27 10:59:19 +0530

    First commit

---
+++ .codedworkflows/ConnectionsManager.cs
@@ -0,0 +1,12 @@
+using UiPath.CodedWorkflows;
+using System;
+
+namespace TestAI
+{
+    public class ConnectionsManager
+    {
+        public ConnectionsManager(ICodedWorkflowsServiceContainer resolver)
+        {
+        }
+    }
+}
---
+++ Workflow.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Data;
+using UiPath.CodedWorkflows;
+using UiPath.Core;
+using UiPath.Core.Activities.Storage;
+using UiPath.Orchestrator.Client.Models;
+
+namespace TestAI
+{
+    public class Workflow : CodedWorkflow
+    {
+        [Workflow]
+        public void Execute()
+        {
+            // To start using services, use IntelliSense (CTRL + Space) to discover the available services:
+            // e.g. system.GetAsset(...)
+
+            // For accessing UI Elements from Object Repository, you can use the Descriptors class e.g:
+            // var screen = uiAutomation.Open(Descriptors.MyApp.FirstScreen);
+            // screen.Click(Descriptors.MyApp.FirstScreen.SettingsButton);
+        }
+    }
+}
